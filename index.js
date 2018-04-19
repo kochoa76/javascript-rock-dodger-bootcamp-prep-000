c15e8a6b8c747f651b427ce983ab84370dc1fd91
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-19 21:56:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     rock.style.top = function 
+     rock.style.top = `${top 
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
